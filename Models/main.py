6149fc6b0a5a19925833cd2e01738c6abc708c33
Hogosha <MrToine01@gmail.com>
2024-12-12 12:06:43 +0100

    filedata

--- Models/main.py
+++ Models/main.py
@@ -2,6 +2,33 @@ import numpy as np
 from functools import lru_cache
 import rdkit
 
+def GetFileData(path:str, ExcludeFirstRow:bool=False, template:list=None) -> list[tuple]:
+    """GetFileData(Path, *[template=list[callables], ExcludeFirstRow:bool]) -> Data:list[tuple]
+    
+    returns the data from a data file.
+    template will be a list of callables that match the data type in each column. 
+    e.g. template=[str, str, int, float, str]"""
+    match path.split(".")[-1]:
+        case "csv":
+            with open(path) as f:
+                data = [tuple(line[:-1].split(",")) for line in f.readlines()]
+        case "txt":
+            with open(path) as f:
+                data = [tuple(line.split()) for line in f.readlines()]
+        case _:
+            return None
+    Final = data[1:] if ExcludeFirstRow else data
+    if isinstance(template, list):
+        return list(map(lambda item:tuple(map(lambda x,func: func(x) ,item, template)), Final)) if bool(reduce(lambda a,b: a and b ,list(map(lambda x:len(template)==len(x) ,Final)))) else ValueError("Template and rows are not of the same length")
+    else: 
+        return Final
+
+testdata = GetFileData("Datasets/test.csv", True, [int, str])
+
+
+def exportCSV(data):
+    return 
+
 def getColumn(matrix, columnNum:int):
     return [x[columnNum] for x in matrix]
 
@@ -25,6 +52,10 @@ def balancedAccuracy(data:list, prediction:list):
     truepos = sum(yhat==1==y for y,yhat in zip(data, prediction))
     trueneg = sum(yhat==0==y for y,yhat in zip(data, prediction))
     return ((truepos/sum(y==1 for y in prediction))+(trueneg/sum(y==0 for y in prediction)))*0.5
+
+
+def Testmodel(data:list[tuple], model):
+    return 
 
 
 if __name__ == "__main__":
